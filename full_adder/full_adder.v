762a246e5e34a726f6a3797fadf944980d426ab7
JOYAL BABU <123290522+Joyal-babu@users.noreply.github.com>
2023-04-03 21:49:08 +0530

    Update full_adder.v

--- full_adder/full_adder.v
+++ full_adder/full_adder.v
@@ -7,7 +7,7 @@ module FA (s, c, x, y, z);
   
   HA H1 ( w1, w2, x, y ); //instantiating halfadder
   HA H2 ( s, w3, w1, z );
-  or ( c, w2, w3 );
+  or G2 ( c, w2, w3 );
   
 endmodule
 
@@ -18,7 +18,7 @@ module HA ( sum, carry, in1, in2 );
   input in1,in2;
   output sum,carry;
   
-  xor G0(sum, in1, in2);
-  and G1(carry, in1, in2);
+  xor G0 (sum, in1, in2);
+  and G1 (carry, in1, in2);
   
 endmodule

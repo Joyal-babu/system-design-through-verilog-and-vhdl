f6c21a288bcbcfb93cd26f0eff54b00bec7e67a6
JOYAL BABU <joyalbabujp21@gmail.com>
2023-04-26 18:47:31 +0530

    Create t_ff.v

---
+++ T flip flop/t_ff.v
@@ -0,0 +1,29 @@
+module t_ff(
+             input t,
+             input clk,
+             input reset,
+             output q
+);
+  
+  wire dt;
+  assign dt=t^q;
+  
+  dff d1(q,dt,clk,reset);//instantiating d flip-flop
+  
+endmodule
+
+//d flip-flop
+
+module dff( output reg q, input d,clk,reset );
+  
+    always @ ( posedge clk )
+    begin
+      
+      if(reset)
+        q<=0;
+      else
+        q<=d;
+      
+    end
+  
+endmodule
